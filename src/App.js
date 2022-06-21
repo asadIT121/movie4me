bb84d13b237f7c3e2997487444e59d9555d3c756
asadIT121 <itasad770@gmail.com>
2022-06-21 15:09:21 +0500

    first

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Button from './data/components/button/Button';
+import Footer from './data/footer/Footer';
+import Header from './data/header/Header';
+
+function App() {
+  return (
+    <>
+        <Header />
+        <Button />
+        <Footer />
+    </>
+  );
+}
+
+export default App;
